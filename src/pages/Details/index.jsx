ac160f92b4488ebcc3bac6be600e6b4b03be27c1
Jamile-Xavier <jamile.x.m@gmail.com>
2023-07-13 11:45:41 -0300

    inicio do front end

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,14 @@
+import { Container } from "./styles";
+
+import { Button } from "../../components/button";
+
+export function Details() {
+  return (
+    <Container>
+      <h1>Hello World</h1>
+      <span>Jamile</span>
+
+      <Button title="Clique aqui" />
+    </Container>
+  );
+}
---
+++ src/styles/theme.js
@@ -0,0 +1,13 @@
+export default {
+  COLORS: {
+    BACKGROUND_PINK: "rgba(255, 133, 155, 0.05)",
+    BACKGROUND_800: "#1C1B1E",
+    BACKGROUND_700: "#262529",
+
+    WHITE: "#F4EDE8",
+    PINK: "#FF859B",
+
+    GRAY_100: "#CAC4CF",
+    GRAY_300: "#948F99",
+  },
+};
