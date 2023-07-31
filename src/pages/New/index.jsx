64a889d384cec4c0df976aca6c441089ad3bf66b
Jamile-Xavier <jamile.x.m@gmail.com>
2023-07-31 11:28:11 -0300

    atualizações integração back e front

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -8,8 +8,54 @@ import { FiArrowLeft } from "react-icons/fi";
 import { Markers } from "../../components/Markers";
 import { Section } from "../../components/Section";
 import { Button } from "../../components/Button";
+import { api } from "../../services/api";
+import { useNavigate } from "react-router-dom";
 
 export function New() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const [tags, setTags] = useState([]);
+  const [newTag, setNewTag] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleAddTag() {
+    setTags((prevState) => [...prevState, newTag]);
+    setNewTag("");
+  }
+
+  function handleRemoveTag() {
+    setTags((prevState) => prevState.filter((tag) => tag !== deleted));
+  }
+
+  async function handleNewNote() {
+    if (!title) {
+      return alert("Digite o título da nota");
+    }
+
+    if (newTag) {
+      return alert(
+        "Você deixou uma tag no campo para adicionar, mas não clicou em adicionar. Clique para adicionar ou deixe o campo vazio."
+      );
+    }
+
+    if (newLink) {
+      return alert(
+        "Você deixou um link no campo para adicionar, mas não clicou em adicionar. Clique para adicionar ou deixe o campo vazio."
+      );
+    }
+
+    await api.post("/notes", {
+      title,
+      description,
+      tags,
+      links,
+    });
+    alert("Nota criada com sucesso");
+    navigate("/");
+  }
+
   return (
     <Container>
       <Header />
@@ -22,19 +68,37 @@ export function New() {
             <h1>Novo Filme</h1>
           </header>
           <Inputs>
-            <Input placeholder="Título" />
+            <Input
+              placeholder="Título"
+              onChange={(e) => setTitle(e.target.value)}
+            />
             <Input placeholder="Sua nota (de 0 a 5)" />
           </Inputs>
-          <Textarea placeholder="Observações" />
+          <Textarea
+            placeholder="Observações"
+            onChange={(e) => setDescription(e.target.value)}
+          />
           <Section title="Marcadores" className="Mark">
             <div className="tags">
-              <Markers value="react" />
-              <Markers value="" isNew placeholder="Novo marcador" />
+              {tags.map((tag, index) => (
+                <Markers
+                  key={String(index)}
+                  value={tag}
+                  onClick={() => handleRemoveTag(tag)}
+                />
+              ))}
+              <Markers
+                isNew
+                placeholder="Nova tag"
+                onChange={(e) => setNewTag(e.target.value)}
+                value={newTag}
+                onClick={handleAddTag}
+              />
             </div>
           </Section>
           <Buttons>
             <Button title="Excluir filme" />
-            <Button title="Salvar alterações" />
+            <Button title="Salvar alterações" onClick={handleNewNote} />
           </Buttons>
         </Form>
       </main>
